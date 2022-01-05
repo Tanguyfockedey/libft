4826a680d8e36a6f5c928f0957839dd7f27bc687
Tanguy Fixe <tanguy@ADMINRG-5TBSHM0.localdomain>
2022-01-05 15:51:38 +0100

    add ft till isprint

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -6,7 +6,26 @@
 /*   By: tfockede <tfockede@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:46:09 by tfockede          #+#    #+#             */
-/*   Updated: 2022/01/03 15:59:52 by tfockede         ###   ########.fr       */
+/*   Updated: 2022/01/05 15:39:24 by tfockede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') ||
+		(c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int c = 'A';
+	int	i;
+
+	i = ft_isalpha(c);
+	printf("%d\n", i);
+}
+*/
