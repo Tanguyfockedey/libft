59b5dbff9ba8a1d79b283bd748de9a8707d569a6
Tanguy Fixe <tanguy@ADMINRG-5TBSHM0.localdomain>
2022-01-06 22:31:54 +0100

    till ft_memmove.c

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -9,4 +9,34 @@
 /*   Updated: 2022/01/06 17:15:11 by tfockede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stddef.h>
 
+void	*ft_memcpy(void *to, const void *from, size_t size)
+{
+	char	*ptr1;
+	const char	*ptr2;
+	size_t	i;
+
+	i = 0;
+	ptr1 = to;
+	ptr2 = from;
+	while(size-- > 0)
+	{
+		ptr1[i] = ptr2[i];
+		i++;
+	}
+	return(to);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char from[] = "This is a string to copy";
+	char to[] = "..................<= to fill";
+
+	printf("%s\n%s\n\n", from, to);
+	ft_memcpy(to, from, 16);
+	printf("%s\n%s\n\n", from, to);
+}
+*/
--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -1,38 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tfockede <tfockede@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/06 15:26:03 by tfockede          #+#    #+#             */
-/*   Updated: 2022/01/06 15:26:03 by tfockede         ###   ########.fr       */
+/*   Created: 2022/01/06 17:56:50 by tfockede          #+#    #+#             */
+/*   Updated: 2022/01/06 17:56:50 by tfockede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
 
-void	*ft_memset(void *block, int c, size_t size)
+void	*ft_memmove(void *to, const void *from, size_t size)
 {
-	char	*ptr;
-	size_t	i;
 
-	i = 0;
-	ptr = block;
-	while(size-- > 0)
-	{
-		ptr[i++] = c;
-	}
-	return (block);
+
+	return(to);
 }
-
-/*
-#include <stdio.h>
-int main(void)
-{
-	char str[] = "This is a string to be erased !\n";
-
-	printf("%s", str);
-	ft_memset(str, '-', 20);
-	printf("%s", str);
-}
-*/
