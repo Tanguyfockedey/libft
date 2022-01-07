ff02074a15c401320a6fc930a3913a900272f11b
Tanguy Fockedey <tfockede@fu-r6-p1.s19.be>
2022-01-07 19:09:14 +0100

    strchr

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tfockede <tfockede@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 18:22:33 by tfockede          #+#    #+#             */
-/*   Updated: 2022/01/07 18:47:12 by tfockede         ###   ########.fr       */
+/*   Updated: 2022/01/07 19:08:51 by tfockede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,28 @@
 char	*ft_strchr(const char *str, int c)
 {
 	size_t i;
-	char	*tmp;
 
-	tmp = str;
 	i = 0;
 	while(str[i])
 	{
 		if(c == str[i])
-			return(&str[i]);
+			return((char *)&str[i]);
 		i++;
 	}
+	if(c == 0)
+		return((char*)&str[i]);
 	return(0);
 }
 
-
+/*
 #include <stdio.h>
 int	main(void)
 {
 	char	str[] = "This is the string to search";
-	int	to_find = 'r';
+	int	to_find = '\0';
 	char	*result;
 
 	result = ft_strchr(str, to_find);
 	printf("%s\n%d\n%s\n", str, to_find, result);
 }
+*/
