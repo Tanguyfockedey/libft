a93e18d537a8c3e2240e10c419ef83afba342643
Tanguy Fockedey <tfockede@fu-r4-p3.s19.be>
2022-01-12 17:01:36 +0100

    strtrim wip

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tfockede <tfockede@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 15:48:03 by tfockede          #+#    #+#             */
-/*   Updated: 2022/01/12 15:50:36 by tfockede         ###   ########.fr       */
+/*   Updated: 2022/01/12 17:00:46 by tfockede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,42 @@
 	removed from the beginning and the end of the string.
 */
 
-char	*ft_strtrim(const char *s1, const char *set)
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	ft_pretrim(const char *str, const char *set)
+{
+	
+}
+
+size_t	ft_endtrim(const char*str, const char *set)
 {
 
+}
+
+char	*ft_strtrim(const char *s1, const char *set)
+{
+	char	*string;
+	size_t	i;
+	size_t	j;
+
+	while(1)
+	{
+		j = 0;
+		while(set[j])
+		{
+			
+			j++;
+		}
+	}
 	
 }
