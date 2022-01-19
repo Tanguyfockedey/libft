4a4f8afdc281d7d4387f648d6a935105798a735c
Tanguy Fixe <tanguy@ADMINRG-5TBSHM0.localdomain>
2022-01-19 15:14:39 +0100

    putendl_fd

--- Libft/ft_putendl_fd.c
+++ Libft/ft_putendl_fd.c
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tfockede <tfockede@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/19 14:42:05 by tfockede          #+#    #+#             */
-/*   Updated: 2022/01/19 14:42:05 by tfockede         ###   ########.fr       */
+/*   Created: 2022/01/19 15:10:53 by tfockede          #+#    #+#             */
+/*   Updated: 2022/01/19 15:10:53 by tfockede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-	Outputs the string ’s’ to the given file descriptor.
+	Outputs the string ’s’ to the given file descriptor, followed by a newline.
 */
 
 #include <stddef.h>
@@ -28,9 +28,10 @@ static size_t	ft_strlen(const char *str)
 
 #include <unistd.h>
 
-void	ft_putstr_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
 }
 
 /*
@@ -43,7 +44,7 @@ int	main(void)
 
 	fd = open("test.txt", O_RDWR | O_CREAT, S_IRWXU);
 	printf("File descriptor is : %d\nString is : %s\n", fd, s);
-	ft_putstr_fd(s, fd);
+	ft_putendl_fd(s, fd);
 	close(fd);
 }
 */
