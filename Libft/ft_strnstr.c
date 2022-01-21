929a23a8077518454041b91cceb07385be9745e7
Tanguy Fockedey <tfockede@fu-r5-p4.s19.be>
2022-01-21 19:03:52 +0100

    corrected ft_strnstr

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tfockede <tfockede@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 16:32:29 by tfockede          #+#    #+#             */
-/*   Updated: 2022/01/11 15:58:08 by tfockede         ###   ########.fr       */
+/*   Updated: 2022/01/21 19:03:05 by tfockede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,15 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	i = 0;
 	while (big[i] && len > 0)
 	{
+		len--;
 		j = 0;
-		while (big[i + j] == little[j])
+		while (big[i + j] == little[j] && j <= len)
 		{
 			if (little[j + 1] == '\0')
 				return ((char*)&big[i]);
 			j++;
 		}	
 		i++;
-		len--;
 	}
 	return (0);
 }
-
-/*
-#include <stdio.h>
-int	main(void)
-{
-	char	str[] = "This is the string to search";
-	char	to_find[] = "string";
-	char	*result;
-
-	result = ft_strnstr(str, to_find, 20);
-	printf("%s\n%s\n%s\n", str, to_find, result);
-}
-*/
