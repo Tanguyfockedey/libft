1656d312f42fba90885531dcc8ffea06209c8a28
Tanguy Fockedey <tfockede@fu-r5-p4.s19.be>
2022-01-21 18:13:48 +0100

    corrected strlcpy

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tfockede <tfockede@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 18:19:58 by tfockede          #+#    #+#             */
-/*   Updated: 2022/01/20 17:34:41 by tfockede         ###   ########.fr       */
+/*   Updated: 2022/01/21 18:13:13 by tfockede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,11 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	i = 0;
 	if (size < 1)
 		return (ft_strlen(src));
-	while (src[i] != '\0' && i < size)
+	while (src[i] != '\0' && i < size - 1)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (i < size)
-		dest[i++] = '\0';
-	dest[size - 1] = '\0';
+	dest[i] = '\0';
 	return (ft_strlen(src));
 }
-
-/*
-#include <stdio.h>
-//#include <bsd/string.h>
-int main(void)
-{
-	char	src[] = "Strlcpy";
-	//char	dest1[15] = "0123456789";
-	char	dest2[15] = "0123456789";
-	int		size = 0;
-
-	while (size < 10)
-	{
-	//printf("%s\t%zu\n", dest1, strlcpy(dest1, src, size));
-	printf("%s\t%zu\n\n", dest2, ft_strlcpy(dest2, src, size));
-	size++;
-	}
-}
-*/
