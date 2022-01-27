154fece984e0d828cef23acd97ee029f9b6da9d4
Tanguy Fockedey <tfockede@fu-r5-p4.s19.be>
2022-01-27 19:25:29 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -16,31 +16,49 @@
 	The array must be ended by a NULL pointer.
 */
 
+#include "libft.h"
 #include <stdlib.h>
 
 static size_t	ft_substr_count(const char *s, char c)
 {
 	size_t	count;
-
-	count = 0;
-	while (*s)
-	{
-		if (*s++ == c)
-			count++;
-	}
-	return (count + 1);
-}
-
-static size_t	ft_substr_len(const char *s, char c)
-{
 	size_t	i;
 
 	i = 0;
-	while ((s[i] != c) && (s[i] != '\0'))
-	{
+	count = 0;
+	if ((c == '\0') && s[0])
+		return (1);
+	if ((c == '\0') && !s[0])
+		return (0);
+	while(s[i] == c)
 		i++;
+	while (s[i])
+	{
+		count++;
+		while ((s[i] != c) && (s[i] != '\0'))
+			i++;
+		while (s[i] == c)
+			i++;
 	}
-	return (i);
+	return (count);
+}
+
+static char	*ft_next_substr(const char *s, char c, size_t *str_index)
+{
+	size_t	str_start;
+	size_t	str_len;
+	char	*str;
+
+	s = &s[*str_index];
+	str_start = 0;
+	str_len = 0;
+	while(s[str_start] == c)
+		str_start++;
+	while ((s[str_start + str_len] != c) && (s[str_start + str_len] != '\0'))
+		str_len++;
+	str = ft_substr(s, str_start, str_len);
+	*str_index = *str_index + str_start + str_len;
+	return (str);
 }
 
 static void	*ft_free(char **result, size_t i)
@@ -53,33 +71,14 @@ static void	*ft_free(char **result, size_t i)
 	return (0);
 }
 
-static char	*ft_substr(const char *str, size_t start, size_t len)
-{
-	char	*substr;
-	size_t	i;
-
-	i = 0;
-	substr = malloc((len + 1) * sizeof(char));
-	if (!substr)
-		return (0);
-	while (i < len)
-	{
-		substr[i] = str[start + i];
-		if (substr[i] == '\0')
-			break ;
-		i++;
-	}
-	substr[i] = '\0';
-	return (substr);
-}
-
 char	**ft_split(const char *s, char c)
 {
-	char	**result;
-	size_t	substr_count;
-	size_t	substr_len;
-	size_t	i;
+	char		**result;
+	size_t		substr_count;
+	size_t		i;
+	size_t		str_i;
 
+	str_i = 0;
 	substr_count = ft_substr_count(s, c);
 	result = malloc((substr_count + 1) * sizeof(char *));
 	if (!result)
@@ -87,11 +86,9 @@ char	**ft_split(const char *s, char c)
 	i = 0;
 	while (i < substr_count)
 	{
-		substr_len = ft_substr_len(s, c);
-		result[i] = ft_substr(s, 0, substr_len);
+		result[i] = ft_next_substr(s, c, &str_i);
 		if (!result[i])
 			return (ft_free(result, i));
-		s = &s[substr_len + 1];
 		i++;
 	}
 	result[substr_count] = NULL;
@@ -102,18 +99,19 @@ char	**ft_split(const char *s, char c)
 #include <stdio.h>
 int	main(void)
 {
-	char *str = "/abc/def/ghi/jkl";
-	char c = '/';
+	char *str = "/aa//bbb";
+	char c = '\0';
 	char **output;
 	int i = 0;
 
 	output = ft_split(str, c);
-	while(output[i] != 0)
+	while(output[i])
 	{
 		printf("%s\n", output[i]);
 		free(output[i]);
 		i++;
 	}
+	printf("%s\n", output[i]);
 	free(output);
 }
 */
